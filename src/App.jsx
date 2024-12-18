0a3d5821c77ed65c9e9015cfc25fa995443f83dc
Oserebameh Augustine, Beckley <beckaustine@outlook.com>
2024-12-18 14:02:23 +0100

    Config setup for layout animation.

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+import { useState, Suspense } from 'react'; 
+import { motion, MotionConfig } from 'motion/react';
+import Scene from './components/3d/Scene'; 
+export default function App() {
+  const [isFullscreen, setFullscreen] = useState(false);
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <MotionConfig>
+      <div 
+      data-is-fullscreen={isFullscreen} 
+      onClick={() => setFullscreen(!isFullscreen)}>
+        <motion.h1 layout> this is somomething</motion.h1>
+        <motion.div layout> 
+          <Suspense>
+            <Scene isFullscreen={isFullscreen} />
+          </Suspense>
+        </motion.div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </MotionConfig>
   )
 }
 
-export default App
