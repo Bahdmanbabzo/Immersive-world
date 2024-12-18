bb22e452e99efff7beb3b61fe97033d96d6cbce0
Oserebameh Augustine, Beckley <beckaustine@outlook.com>
2024-12-18 14:09:19 +0100

    Create Scene with layout camera.

---
+++ src/components/3d/Scene.jsx
@@ -0,0 +1,20 @@
+import { MotionCanvas, LayoutCamera } from "framer-motion-3d";
+
+export default function Scene({isFullscreen}) {
+    return (
+        <MotionCanvas>
+            <LayoutCamera
+                initial={false}
+                animate = {
+                    isFullscreen ? {
+                        x:10, 
+                        y:5, 
+                        z:10, 
+                        fov:30, 
+                    } : {
+                        x:15, y:0.25, z:0, fov:10
+                    }
+                } />
+        </MotionCanvas>
+    )
+}
