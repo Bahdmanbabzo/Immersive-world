c9e697b3c737441fc7b67d799e90cd2b0bf0106d
Oserebameh Augustine, Beckley <beckaustine@outlook.com>
2024-12-19 21:50:18 +0100

    Update Model.jsx

--- src/components/3d/Model.jsx
+++ src/components/3d/Model.jsx
@@ -1,5 +1,5 @@
 import { useLoader } from "@react-three/fiber"
-import { GLTFLoader } from "three/examples/jsm/Addons.js"
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader"
 
 export default function Model() {
     const gltf = useLoader(GLTFLoader, "/rubiks_cube.glb"); 
