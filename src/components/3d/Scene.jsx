bb7ca4d88f9024f5d487743fb4f5a445a692c194
Oserebameh Augustine, Beckley <beckaustine@outlook.com>
2024-12-20 08:59:30 +0100

    Portal material shader from drei.

--- src/components/3d/Scene.jsx
+++ src/components/3d/Scene.jsx
@@ -1,30 +1,19 @@
 import { Canvas } from '@react-three/fiber';
-import { OrbitControls } from '@react-three/drei';
+import { OrbitControls, MeshPortalMaterial, RoundedBox } from '@react-three/drei';
 import Model from './Model'; 
 import Lights from "./Lights";
 
 export default function Scene({isFullscreen}) {
     // useMemo(() => extend({THREE}), []);
     return (
-        <Canvas>
-            {/* <AmbientLight intensity={0.5} /> */}
-            <ambientLight intensity={0.2} />
-            {/* <LayoutCamera
-                initial={false}
-                animate = {
-                    isFullscreen ? {
-                        x:10, 
-                        y:5, 
-                        z:10, 
-                        fov:30, 
-                    } : {
-                        x:15, y:0.25, z:0, fov:10
-                    }
-                } /> */}
-            {/* <Lights isFullscreen={isFullscreen} /> */}
-            <Model />   
-            <color attach="background" args={['#000000']} />
-            <OrbitControls />
+        <Canvas className='border-green-500 border-4'><OrbitControls />
+            <color attach={"background"} args={['#000000']}/>
+            <mesh>
+                <planeGeometry />
+                <MeshPortalMaterial>
+                    <Model />
+                </MeshPortalMaterial>
+            </mesh>
         </Canvas>
     )
 }
