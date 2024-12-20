60bd39a9a0d7a04d0febcb6a7d9342e6c93ceed6
Oserebameh Augustine, Beckley <beckaustine@outlook.com>
2024-12-20 18:18:57 +0100

    Give the model an identifiable name in scene.

--- src/components/3d/Model.jsx
+++ src/components/3d/Model.jsx
@@ -3,7 +3,8 @@ import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader"
 
 export default function Model() {
     const gltf = useLoader(GLTFLoader, "/rubiks_cube.glb"); 
+    console.log(gltf.scene)
     return(
-        <primitive object={gltf.scene} />
+        <primitive object={gltf.scene} name="model" />
     )
 }
