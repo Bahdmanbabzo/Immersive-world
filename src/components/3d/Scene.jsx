e9f3464b5b0099114ee6e7a78ef9a6a1bdcaba73
Oserebameh Augustine, Beckley <beckaustine@outlook.com>
2024-12-20 17:12:31 +0100

    Update portal set on every mount.

--- src/components/3d/Scene.jsx
+++ src/components/3d/Scene.jsx
@@ -1,10 +1,18 @@
 import { Canvas } from '@react-three/fiber';
 import { OrbitControls, MeshPortalMaterial, RoundedBox } from '@react-three/drei';
+import { animate } from 'motion';
 import * as THREE from 'three';
 import Model from './Model'; 
 import Lights from "./Lights";
+import { useEffect, useRef, useState } from 'react';
 
 export default function Scene({isFullscreen}) {
+    const [portalState, setPortalState] = useState();
+    const portalRef = useRef();
+    useEffect(() => {
+        if(isFullscreen) {
+        } 
+    },[portalState, isFullscreen])
     // useMemo(() => extend({THREE}), []);
     return (
         <Canvas 
@@ -16,6 +24,7 @@ export default function Scene({isFullscreen}) {
                 
                 <planeGeometry args={[4,5]} />
                 <MeshPortalMaterial 
+                    ref={setPortalState}
                     side={THREE.DoubleSide} 
                     blend={ isFullscreen ? 1: 0}
                 >
