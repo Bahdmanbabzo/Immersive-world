e2e46eb5a32f63de1201555b77ce9cff7caf3cd7
Oserebameh Augustine, Beckley <beckaustine@outlook.com>
2024-12-20 23:25:54 +0100

    Update Scene.jsx

--- src/components/3d/Scene.jsx
+++ src/components/3d/Scene.jsx
@@ -1,37 +1,32 @@
 import { Canvas } from '@react-three/fiber';
-import { OrbitControls, MeshPortalMaterial, RoundedBox } from '@react-three/drei';
+import { MeshPortalMaterial, Float, CameraControls} from '@react-three/drei';
 import { animate } from 'motion';
 import * as THREE from 'three';
 import Model from './Model'; 
 import Lights from "./Lights";
+import Camera from './Cameras';
 import { useEffect, useRef, useState } from 'react';
 
 export default function Scene({isFullscreen}) {
-    const [portalState, setPortalState] = useState();
-    const portalRef = useRef();
-    useEffect(() => {
-        if(isFullscreen) {
-        } 
-    },[portalState, isFullscreen])
     // useMemo(() => extend({THREE}), []);
     return (
         <Canvas 
         className='border-green-500 border-4 h-full w-full' 
-        camera={{position: [0, 0, 5]}}
+        camera={{position: [0, -2, 10]}}
         >
-            <OrbitControls />
             <mesh>
-                
                 <planeGeometry args={[4,5]} />
                 <MeshPortalMaterial 
-                    ref={setPortalState}
                     side={THREE.DoubleSide} 
                     blend={ isFullscreen ? 1: 0}
                 >
-                    <Model />
+                    <Float>
+                        <Model/>
+                    </Float>
                     <color attach={"background"} args={['#000000']}/>
                 </MeshPortalMaterial>
             </mesh>
+            <Camera isFullscreen={isFullscreen} model={<Model />} />
         </Canvas>
     )
 }
