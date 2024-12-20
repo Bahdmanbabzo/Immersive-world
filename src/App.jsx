0ebf6aad1ad9f06cc410db5c1f3d749770db2300
Oserebameh Augustine, Beckley <beckaustine@outlook.com>
2024-12-20 23:26:05 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,16 @@ import { motion, MotionConfig } from 'motion/react';
 import Scene from './components/3d/Scene'; 
 export default function App() {
   const [isFullscreen, setFullscreen] = useState(false);
+  console.log(isFullscreen);
   return (
     <MotionConfig>
       <div 
-      className='relative'
+      className='h-screen w-screen grid place-items-center'
       data-is-fullscreen={isFullscreen} 
       onClick={() => setFullscreen(!isFullscreen)}>
-        <motion.h1 layout className='absolute top-0 text-white font-semibold'> this is somomething</motion.h1>
-        <motion.div className='h-screen w-screen fixed' layout> 
-          <Suspense>
+        {/* <motion.h1 layout className='absolute top-0 text-white font-semibold'> this is somomething</motion.h1> */}
+        <motion.div className='h-screen w-screen'> 
+          <Suspense className="h-full w-full" fallback="loading...">
             <Scene isFullscreen={isFullscreen} />
           </Suspense>
         </motion.div>
