a9332180669a1c284bf61ded2d9b6604912f88d1
Oserebameh Augustine, Beckley <beckaustine@outlook.com>
2024-12-21 08:36:53 +0100

    Refactored camera animation with framer motion

--- src/components/3d/Cameras.jsx
+++ src/components/3d/Cameras.jsx
@@ -1,39 +1,18 @@
 import {CameraControls } from '@react-three/drei';
 import { useEffect, useRef } from 'react';
+import { animate } from 'motion';
 import { useThree } from '@react-three/fiber';
 import * as THREE from 'three';
 
 
-export default function Camera({isFullscreen,model}) {
-    // const { scene } = useThree();
-    // console.log(scene.getObjectByName('model'));
-    const cameraRef = useRef(); 
-    useEffect(() => {
-        if(isFullscreen){
-            const targetPosition = new THREE.Vector3(0,0,0); 
-            // model.getWorldPosition(targetPosition); 
-            cameraRef.current.setLookAt(
-                0,
-                0,
-                5,
-                targetPosition.x,
-                targetPosition.y,
-                targetPosition.z,
-                true
-            )
-        }else {
-            cameraRef.current.setLookAt(
-                0,
-                0,
-                10,
-                0,
-                0,
-                0,
-                true
-            )
-        }
-    }, [isFullscreen])
-    return (
-        <CameraControls ref={cameraRef} />
-    );
-};
+export default function Camera({isFullscreen}) {
+    const { camera } = useThree();
+    const cameraPosition = camera.position; 
+    console.log(cameraPosition); 
+    useEffect(() =>{
+        console.log("the camera effect"); 
+        animate(cameraPosition, {z: isFullscreen ? 5: 10, duration: 5, ease: 'easeInOut'});
+    },[isFullscreen])
+
+    return(null)
+}
