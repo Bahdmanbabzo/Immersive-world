ee2a7ea8965b060dec70284d0fb1dfa06bace852
Oserebameh Augustine, Beckley <beckaustine@outlook.com>
2024-12-22 16:54:40 +0100

    Pass ref to be accessed in scene.

--- src/components/3d/Portal.jsx
+++ src/components/3d/Portal.jsx
@@ -1,23 +1,22 @@
+import React, { useEffect, useRef, forwardRef } from 'react';
 import { MeshPortalMaterial, Float } from '@react-three/drei';
-import { useEffect, useRef } from 'react';
-import { animate } from 'motion';
 import * as THREE from 'three';
 
-export default function Portal({isFullscreen, children}) {
-    const portalRef = useRef();
+const Portal = forwardRef(function Portal({ isFullscreen, children }, ref) {
     return (
         <mesh>
-        <planeGeometry args={[4,5]} />
-        <MeshPortalMaterial 
-            ref={portalRef}
-            side={THREE.DoubleSide} 
-            blend={ isFullscreen ? 1: 0}
-        >
-            <Float>
-               {children}
-            </Float>
-            <color attach={"background"} args={['#000000']}/>
-        </MeshPortalMaterial>
-    </mesh>
-    )
-}
+            <planeGeometry args={[4, 5]} />
+            <MeshPortalMaterial 
+                ref={ref}
+                side={THREE.DoubleSide} 
+                blend={isFullscreen ? 1 : 0}
+            >
+                <Float>
+                    {children}
+                </Float>
+            </MeshPortalMaterial>
+        </mesh>
+    );
+});
+
+export default Portal;
