250dc937956d35b8aabe194f3d5e4785943d23e2
Oserebameh Augustine, Beckley <beckaustine@outlook.com>
2024-12-26 14:05:29 +0100

    Back to working portal transition.

--- src/components/3d/Portal.jsx
+++ src/components/3d/Portal.jsx
@@ -1,26 +1,4 @@
-<<<<<<< HEAD
-import React, { useEffect, useRef, forwardRef } from 'react';
-import { MeshPortalMaterial, Float, Environment } from '@react-three/drei';
-import { animate } from 'motion';
-import * as THREE from 'three';
 
-export default function Portal({ isFullscreen, children }) {
-    return (
-        <mesh>
-            <planeGeometry args={[10, 10]} />
-            {/* <meshStandardMaterial color="red" /> */}
-            <MeshPortalMaterial 
-                // side={THREE.DoubleSide} 
-                blend={isFullscreen ? 1 : 0}
-            >   
-                <Environment preset="night" />
-                <Float>
-                    {children}
-                </Float>
-            </MeshPortalMaterial>
-        </mesh>
-    )};
-=======
 import { MeshPortalMaterial, Float } from '@react-three/drei';
 import { useEffect, useRef } from 'react';
 import { animate } from 'motion';
@@ -44,4 +22,3 @@ export default function Portal({isFullscreen, children}) {
     </mesh>
     )
 }
->>>>>>> parent of ee2a7ea (Pass ref to be accessed in scene.)
