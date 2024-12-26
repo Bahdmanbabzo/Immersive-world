7ec71024036f1352ab90c88c84d62acc1c47c0df
Oserebameh Augustine, Beckley <beckaustine@outlook.com>
2024-12-26 15:32:38 +0100

    Smoothly animate interpolate the blend value of portal.

--- src/components/3d/Portal.jsx
+++ src/components/3d/Portal.jsx
@@ -1,18 +1,27 @@
 
 import { MeshPortalMaterial, Float } from '@react-three/drei';
-import { useEffect, useRef } from 'react';
-import { animate } from 'motion';
+import { damp } from 'maath/easing'
+import { useRef } from 'react';
 import * as THREE from 'three';
+import { useFrame } from '@react-three/fiber';
 
 export default function Portal({isFullscreen, children}) {
     const portalRef = useRef();
+    //smoothly animate the blend value of the portal
+    useFrame(({delta})=> {
+        if(isFullscreen){
+            damp(portalRef.current, "blend", 1,0.5, delta)
+        }else {
+            damp(portalRef.current, "blend", 0,0.5, delta)
+        }
+    })
     return (
         <mesh>
         <planeGeometry args={[4,5]} />
         <MeshPortalMaterial 
             ref={portalRef}
             side={THREE.DoubleSide} 
-            blend={ isFullscreen ? 1: 0}
+            blend={0}
         >
             <Float>
                {children}
