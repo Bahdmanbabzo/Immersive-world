db7ba6694c0e70cc6934a572fa11c69034dabb1a
Oserebameh Augustine, Beckley <beckaustine@outlook.com>
2024-12-26 14:03:10 +0100

    Update Portal.jsx

--- src/components/3d/Portal.jsx
+++ src/components/3d/Portal.jsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React, { useEffect, useRef, forwardRef } from 'react';
 import { MeshPortalMaterial, Float, Environment } from '@react-three/drei';
 import { animate } from 'motion';
@@ -19,3 +20,28 @@ export default function Portal({ isFullscreen, children }) {
             </MeshPortalMaterial>
         </mesh>
     )};
+=======
+import { MeshPortalMaterial, Float } from '@react-three/drei';
+import { useEffect, useRef } from 'react';
+import { animate } from 'motion';
+import * as THREE from 'three';
+
+export default function Portal({isFullscreen, children}) {
+    const portalRef = useRef();
+    return (
+        <mesh>
+        <planeGeometry args={[4,5]} />
+        <MeshPortalMaterial 
+            ref={portalRef}
+            side={THREE.DoubleSide} 
+            blend={ isFullscreen ? 1: 0}
+        >
+            <Float>
+               {children}
+            </Float>
+            <color attach={"background"} args={['#000000']}/>
+        </MeshPortalMaterial>
+    </mesh>
+    )
+}
+>>>>>>> parent of ee2a7ea (Pass ref to be accessed in scene.)
